4da15432a0edc21427f3077d1ed25a65b68bba94
Daniel Robens <r00t@Mac.fritz.box>
2025-10-15 19:40:48 +0200

    chore: clean up

--- sveltekit-app/src/lib/utils/index.ts
+++ sveltekit-app/src/lib/utils/index.ts
@@ -1,10 +1,3 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
 export function formatDate(date: string) {
   return new Date(date).toLocaleDateString('en-US', {
     month: 'long',
