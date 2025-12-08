263ea607c9c91d08b742a0127ca10cc07b3eb34c
Daniel Robens <daniel.robens@googlemail.com>
2025-12-08 21:06:23 +0100

    feat(Accordion-item): draft details

--- sveltekit-app/src/lib/components/AccordionItem.svelte
+++ sveltekit-app/src/lib/components/AccordionItem.svelte
@@ -1,77 +1,87 @@
-<div class="
-  accordion-item
-  flex flex-col gap-4
-  {className}
-  ">
-  <div class="accordion-header" id={`heading-${id}`}>
-    <button
-      class="accordion-button text-start w-full py-5"
-      class:collapsed={! expanded}
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target={`#target-${id}`}
-      aria-expanded={expanded}
-      aria-controls={`target-${id}`}
-      onclick={toggle}
-      >
-      <div class="grid grid-cols-12">
-        {#if index}
-          <div class="col-start-1 col-span-2 {expanded ? 'gradient-primary' : ''} font-bold leading-none select-none flex items-center">
-            {index < 10 ? `0${index}` : index}
-          </div>
-        {/if}
-        <div class="col-start-{index ? 3 : 2} col-span-{index ? '7' : '8'} flex items-center">
-          {@render title()}
+<details
+  bind:this={details} 
+  class="
+    accordion-item
+    flex flex-col
+    {closing ? 'is-closing' : ''}
+    {className}
+  "
+  open={expanded ? true : undefined}
+  >
+  <summary
+    class="accordion-header py-5"
+    id={`heading-${id}`}
+    onclick={toggle}
+    aria-expanded={expanded}
+    aria-controls={`target-${id}`}
+    >
+    <div class="grid grid-cols-12">
+      {#if index}
+        <div class="col-start-1 col-span-2 {expanded ? 'gradient-primary' : ''} font-bold leading-none select-none flex items-center">
+          {index < 10 ? `0${index}` : index}
         </div>
-        <div class="col-start-11 col-span-2 grid items-center ms-auto">
-          <svg
-            width="30"
-            height="17"
-            viewBox="0 0 30 17"
-            class="accordion-toggle transition-transform duration-300 ease-in-out"
-            class:rotate-180={expanded}
-            style="transform-origin: center;"
-            >
-            <path d="M15.1914 16.9414C14.7168 16.9414 14.3125 16.748 13.9609 16.3965L1.14648 3.30078C0.830078 2.98438 0.654297 2.58008 0.654297 2.12305C0.654297 1.19141 1.375 0.470703 2.30664 0.470703C2.78125 0.470703 3.18555 0.646484 3.50195 0.945312L15.209 12.916L26.8984 0.945312C27.2148 0.646484 27.6367 0.470703 28.0762 0.470703C29.0078 0.470703 29.7285 1.19141 29.7285 2.12305C29.7285 2.58008 29.5527 2.98438 29.2539 3.2832L16.4395 16.3965C16.123 16.748 15.666 16.9414 15.1914 16.9414Z" />
-          </svg>
-        </div>
+      {/if}
+      <div class="col-start-{index ? 3 : 2} col-span-{index ? '7' : '8'} flex items-center">
+        {@render title()}
       </div>
-    </button>
-  </div>
-  {#if expanded}
-    <div
-      class="accordion-collapse overflow-hidden"
-      id={`target-${id}`}
-      aria-labelledby={`heading-${id}`}
-      data-bs-parent={parent}
-      in:slide={{ duration: 400, easing: cubicOut }}
-      out:slide={{ duration: 300, easing: cubicOut }}
-      >
-      <div class="accordion-body">
-        {@render body()}
+      <div class="col-start-11 col-span-2 grid items-center ms-auto">
+        <svg
+          width="30"
+          height="17"
+          viewBox="0 0 30 17"
+          class="accordion-toggle transition-transform duration-300 ease-in-out"
+          class:rotate-180={expanded}
+          style="transform-origin: center;"
+          >
+          <path d="M15.1914 16.9414C14.7168 16.9414 14.3125 16.748 13.9609 16.3965L1.14648 3.30078C0.830078 2.98438 0.654297 2.58008 0.654297 2.12305C0.654297 1.19141 1.375 0.470703 2.30664 0.470703C2.78125 0.470703 3.18555 0.646484 3.50195 0.945312L15.209 12.916L26.8984 0.945312C27.2148 0.646484 27.6367 0.470703 28.0762 0.470703C29.0078 0.470703 29.7285 1.19141 29.7285 2.12305C29.7285 2.58008 29.5527 2.98438 29.2539 3.2832L16.4395 16.3965C16.123 16.748 15.666 16.9414 15.1914 16.9414Z" />
+        </svg>
       </div>
     </div>
-  {/if}
-</div>
+  </summary>
+  <div
+    class="accordion-collapse overflow-hidden"
+    id={`target-${id}`}
+    aria-labelledby={`heading-${id}`}
+    data-bs-parent={parent}
+    onanimationend={onAnimationEnd}
+    >
+    <div class="accordion-body">
+      {@render body()}
+    </div>
+  </div>
+</details>
 
-<script>
-import { slide } from 'svelte/transition';
-import { cubicOut } from 'svelte/easing';
-
+<script lang="ts">
 let {
   id,
   index,
   expanded = $bindable(false),
+  closing = $bindable(false),
   parent,
   title,
   body,
   class: className = '',
 } = $props();
 
-function toggle() {
-  console.log(`AccordionItem ${id} toggled`);
-  
-  expanded = !expanded;
+let details = $state<HTMLElement>();
+
+function toggle(event: Event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  if (expanded) {
+    closing = true;
+  }
+  else {
+    expanded = true;
+  }
+}
+
+function onAnimationEnd(event: AnimationEvent) {
+  if (event.animationName.endsWith('-close')) {
+    expanded = false;
+    closing = false;
+  }
 }
 </script>
 
@@ -92,9 +102,39 @@ function toggle() {
     }
   }
   
-  .accordion-item {
+  /* .accordion-item {
     .col-start-1 {
       transition: all 0.3s ease-in-out;
     }
+  } */
+  details summary {
+    list-style: none;
+  }
+  @keyframes open {
+    0% {
+      clip-path: inset(0% 0% 100% 0%);
+    }
+    100% {
+      clip-path: inset(0% 0% 0% 0%);
+    }
+  }
+
+  /* closing animation */
+  @keyframes close {
+    0% {
+      clip-path: inset(0% 0% 0% 0%);
+    }
+    100% {
+      clip-path: inset(0% 0% 100% 0%);
+    }
+  }
+
+  details[open] summary ~ .accordion-collapse {
+    animation: open 400ms cubic-bezier(0.55, 0, 0.1, 1)
+  }
+
+  /* closing class */
+  details.is-closing summary ~ .accordion-collapse {
+    animation: close 300ms cubic-bezier(0.55, 0, 0.1, 1)
   }
 </style>
